4999c966457ada092e888ae70d34ff35c224d3aa
Ali ATIL <alihaydar.atil@gmail.com>
2018-03-15 23:52:56 +0300

    stdlib

--- src/stdlib.c
+++ src/stdlib.c
@@ -1,23 +1,24 @@
+#include "string.h"
 #define MAX_CHAR 11
 
 char numbers[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
 void itoa(int number, char* str, int base) {
-	int count = 0, remainder, quotinent, i, temp;
+	int i = 0, sign;
+
+	if ((sign = number) < 0) {
+		number = -number; // make number positive
+	}
 
 	do {
-		quotinent = number / base;
-		remainder = number % base;
-		str[count] = numbers[remainder];
-		number = quotinent;
-		count++;
-	} while (quotinent != 0);
+		str[i++] = numbers[number % base];
+	} while ((number /= base) > 0);
 
-	str[count] = '\0';
-
-	for (i = 0; i < count / 2; i++) {
-		temp = str[i];
-		str[i] = str[count - 1 - i];
-		str[count - 1 - i] = temp;
+	if (sign < 0) {
+		str[i++] = '-';
 	}
+
+	str[i] = '\0';
+
+	reverse(str);
 }
