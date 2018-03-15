d5a9395d9a8b87e7ec86e2a51ef08493e1a0b6db
Ali ATIL <alihaydar.atil@gmail.com>
2018-03-15 23:53:07 +0300

    string.h

--- src/string.c
+++ src/string.c
@@ -1,14 +1,20 @@
 
-static const char STRING_TERMINATOR = 0x00;
-
-
-int strlength(const char* string){
-	int len=0;
-	while(string[len]){
+int strlength(const char* string) {
+	int len = 0;
+	while (string[len]) {
 		len++;
 	}
 	return len;
-
 }
 
+void reverse(char string[]) {
+	int i, j;
+	char temp;
+
+	for (i = 0, j = strlength(string) - 1; i < j; i++, j--) {
+		temp = string[i];
+		string[i] = string[j];
+		string[j] = temp;
+	}
+}
 
